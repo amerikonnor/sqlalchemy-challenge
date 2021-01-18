14dd7751f26930e1d84a8a7e456e8161cde9e935
Connor MacKenzie <cmm224@case.edu>
2021-01-18 03:46:55 -0500

    just a homepage

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-app = Flash(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'Welcome to my homepage!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
