bd54195cfd5fc9d9712e408a90e8e0ff17645439
Connor MacKenzie <cmm224@case.edu>
2021-01-18 04:03:58 -0500

    added a notebook to work in, precipitation page working

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+#############################
+#Database Setup
+#############################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+Base = automap_base()
+
+Base.prepare(engine,reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+##########################
+#Flask Setup
+##########################
 app = Flask(__name__)
 
 @app.route("/")
@@ -21,7 +34,11 @@ def index():
 
 @app.route('/api/v1.0/precipitation')
 def prcp():
-    pass
-
+    session = Session(engine)
+    results = session.query(Measurement.date,Measurement.prcp).all()
+    session.close()
+    prcp_dict = dict(results)
+    return jsonify(prcp_dict)
+    
 if __name__ == "__main__":
     app.run(debug=True)
