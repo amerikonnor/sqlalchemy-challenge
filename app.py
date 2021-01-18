429cdbae65e4ef746055371105db1f62c2cc6ce5
Connor MacKenzie <cmm224@case.edu>
2021-01-18 03:51:35 -0500

    imports and paths

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Welcome to my homepage!'
+    return(
+        f'Welcome to my Climate API'
+        f'Available routes:'
+        f'/api/v1.0/precipitation'
+        f'/api/v1.0/stations'
+        f'/api/v1.0/tobs'
+        f'/api/v1.0/<start>'
+        f'/api/v1.0/<start>/<end>'
+    )
+
+@app.route('/api/v1.0/precipitation')
+def prcp():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
