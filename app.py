6e495b3992a506f3fd572664cba3532e09af628b
Connor MacKenzie <cmm224@case.edu>
2021-01-18 04:08:44 -0500

    home page formatting

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return(
-        f'Welcome to my Climate API'
-        f'Available routes:'
-        f'/api/v1.0/precipitation'
-        f'/api/v1.0/stations'
-        f'/api/v1.0/tobs'
-        f'/api/v1.0/<start>'
-        f'/api/v1.0/<start>/<end>'
+        f'Welcome to my Climate API<br/>'
+        f'Available routes:<br/>'
+        f'/api/v1.0/precipitation<br/>'
+        f'/api/v1.0/stations<br/>'
+        f'/api/v1.0/tobs<br/>'
+        f'/api/v1.0/#startdate#<br/>'
+        f'/api/v1.0/#startdate#/#enddate#'
     )
 
 @app.route('/api/v1.0/precipitation')
@@ -39,6 +39,13 @@ def prcp():
     session.close()
     prcp_dict = dict(results)
     return jsonify(prcp_dict)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    session = Session(engine)
+    results = session.query(Station.name).all()
+    session.close()
+    return jsonify(results)
     
 if __name__ == "__main__":
     app.run(debug=True)
